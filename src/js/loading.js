64711d7a47f2cf5a27e559ce1b066c659a334327
Vitalii <denberry81@gmail.com>
2019-04-12 16:14:58 +0300

    add loading text and dots

---
+++ src/js/loading.js
@@ -0,0 +1,5 @@
+function hideLoader() {
+
+}
+
+export { hideLoader };
--- src/js/main.js
+++ src/js/main.js
@@ -1,91 +1,90 @@
 import "../scss/main.scss";
 import "./vh-fix";
-import "./loader";
-import "./3dmodel";
-import * as model from "./3dmodel";
+// import "./3dmodel";
+// import * as model from "./3dmodel";
 
-window.addEventListener("load", function () {
-  let container = document.querySelector(".container");
+// window.addEventListener("load", function () {
+//   let container = document.querySelector(".container");
 
-  let navs = document.querySelectorAll(".nav__item");
+//   let navs = document.querySelectorAll(".nav__item");
 
-  let imgWrapper = document.querySelector(".gallery__images");
+//   let imgWrapper = document.querySelector(".gallery__images");
 
-  let images = document.querySelectorAll(".gallery__img");
+//   let images = document.querySelectorAll(".gallery__img");
 
-  let video = document.querySelector(".trailer__video");
+//   let video = document.querySelector(".trailer__video");
 
-  let mobileLandscape = window.matchMedia("(max-width: 767px) and (orientation: landscape)");
-  let mobilePortrait = window.matchMedia("(max-width: 599px) and (orientation: portrait)");
-
-
-  mobileLandscape.addListener(landscapeHandler);
-  mobilePortrait.addListener(portraitHandler);
-
-  function landscapeHandler(evt) {
-    if (evt.matches) {
-      model.stopAnimation();
-      model.idleAnimation();
-    }
-  }
-
-  function portraitHandler(evt) {
-    let current = container.dataset.active;
-    if (evt.matches && current != "yeezy") {
-      model.stopAnimation();
-    }
-  }
+//   let mobileLandscape = window.matchMedia("(max-width: 767px) and (orientation: landscape)");
+//   let mobilePortrait = window.matchMedia("(max-width: 599px) and (orientation: portrait)");
 
 
-  navs.forEach(nav => {
-    nav.addEventListener("click", () => {
-      let current = container.dataset.active;
+//   mobileLandscape.addListener(landscapeHandler);
+//   mobilePortrait.addListener(portraitHandler);
 
-      if (nav.dataset.nav != current) {
-        setNavigation(nav.dataset.nav, current);
-        container.dataset.active = nav.dataset.nav;
+//   function landscapeHandler(evt) {
+//     if (evt.matches) {
+//       model.stopAnimation();
+//       model.idleAnimation();
+//     }
+//   }
 
-      }
-    });
-  });
+//   function portraitHandler(evt) {
+//     let current = container.dataset.active;
+//     if (evt.matches && current != "yeezy") {
+//       model.stopAnimation();
+//     }
+//   }
 
-  images.forEach((img, index) => {
-    img.addEventListener("click", () => {
 
-      imgWrapper.dataset.img = index >= images.length - 1 ? 1 : index + 2;
+//   navs.forEach(nav => {
+//     nav.addEventListener("click", () => {
+//       let current = container.dataset.active;
 
-    });
-  });
+//       if (nav.dataset.nav != current) {
+//         setNavigation(nav.dataset.nav, current);
+//         container.dataset.active = nav.dataset.nav;
 
-  video.src = video.dataset.src;
+//       }
+//     });
+//   });
 
-  video.addEventListener('click', function () {
-    if (video.paused || video.ended) {
-      video.play();
-    }
-    else {
-      video.pause();
-    }
-  })
+//   images.forEach((img, index) => {
+//     img.addEventListener("click", () => {
 
-  function setNavigation(nav, current) {
-    if (nav != "yeezy") {
-      model.stopAnimation();
-    }
-    else {
-      model.stopAnimation();
-      model.idleAnimation();
-    }
-    if (current == "trailer" && (!video.paused || !video.ended)) {
-      video.pause();
-    }
-    else if (nav == "trailer" && current != nav) {
-      video.currentTime = 0;
-      video.play();
-    }
-  }
+//       imgWrapper.dataset.img = index >= images.length - 1 ? 1 : index + 2;
 
-});
+//     });
+//   });
+
+//   video.src = video.dataset.src;
+
+//   video.addEventListener('click', function () {
+//     if (video.paused || video.ended) {
+//       video.play();
+//     }
+//     else {
+//       video.pause();
+//     }
+//   })
+
+//   function setNavigation(nav, current) {
+//     if (nav != "yeezy") {
+//       model.stopAnimation();
+//     }
+//     else {
+//       model.stopAnimation();
+//       model.idleAnimation();
+//     }
+//     if (current == "trailer" && (!video.paused || !video.ended)) {
+//       video.pause();
+//     }
+//     else if (nav == "trailer" && current != nav) {
+//       video.currentTime = 0;
+//       video.play();
+//     }
+//   }
+
+// });
 
 
 
